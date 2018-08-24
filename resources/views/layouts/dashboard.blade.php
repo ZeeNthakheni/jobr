e361e9e0c7a3682b991b6d9b14ccce21c185ef27
ZeeNthakheni <30797347+ZeeNthakheni@users.noreply.github.com>
2018-08-24 13:49:39 +0200

    Added template

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Purple Admin</title>
+
+  
+
+  <!-- plugins:css -->
+  <link href="{{ asset('iconfonts/mdi/css/materialdesignicons.min.css')}}" rel="stylesheet">
+  <link href="{{ asset('css/app.vendor.bundle.base.css') }}" rel="stylesheet">
+  <!-- endinject -->
+
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <!-- endinject -->
+
+</head>
+
+<body>
+  <div class="container-scroller">
+    <!-- partial:../../partials/_navbar.html -->
+        @include('partials.navbar')
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:../../partials/_sidebar.html -->
+      @include('partials.sidebar')
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+            @yield('content')
+        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:../../partials/_footer.html -->
+        @include('partials.footer')
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <!-- Scripts -->
+  <script src="{{ asset('js/vendor.bundle.base.js') }}" defer></script>
+  
+  <!-- Scripts -->
+  <script src="{{ asset('js/vendor.bundle.addons.js') }}" defer></script>
+  
+  <!-- endinject -->
+  <!-- Plugin js for this page-->
+  <!-- End plugin js for this page-->
+  <!-- inject:js -->
+  <script src="{{ asset('js/off-canvas.js') }}" defer></script>
+  <script src="{{ asset('js/misc.js') }}" defer></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <!-- End custom js for this page-->
+</body>
+
+</html>
